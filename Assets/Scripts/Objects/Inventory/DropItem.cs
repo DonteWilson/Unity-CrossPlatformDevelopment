79bda969a8bd2c22b45ba41c498d9db076a17959
Donte Wilson <JittNA@aol.com>
2017-05-16 17:53:46 -0500

    LootTable
    added loottable script

---
+++ Assets/Scripts/Objects/Inventory/DropItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropItem
+{
+    [Range(1, 100)]
+    public float DropPercentage;
+
+    public Item DroppedItem;
+    
+}
---
+++ Assets/Scripts/Objects/Inventory/LootTable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "LootTable")]
+public class LootTable : ScriptableObject
+{
+
+    public List<DropItem> EnemyDrop = new List<DropItem>();
+    public int Percentage;
+    public GameObject _object;
+
+    public DropItem ItemDrop()
+    {
+
+        SortbyPercentage();
+        return null;
+    }
+
+
+    public void SortbyPercentage()
+    {
+        EnemyDrop = EnemyDrop.OrderBy(i => i.DropPercentage).ToList();
+    }
+}
+
+
+
+
+#if UNITY_EDITOR
+
+
+#endif
